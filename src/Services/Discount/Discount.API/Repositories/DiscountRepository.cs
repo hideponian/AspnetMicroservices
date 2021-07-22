b51eb66630e04046ded8ae6a4c64ed5cc3f66221
dev01-at-120106085688 <hideponianpublic@gmail.com>
2021-07-23 07:13:43 +0900

    h

---
+++ src/Services/Discount/Discount.API/Repositories/DiscountRepository.cs
@@ -0,0 +1,30 @@
+﻿using Dapper;
+using Discount.API.Entities;
+using Microsoft.Extensions.Configuration;
+using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Discount.API.Repositories
+{
+    public class DiscountRepository : IDiscountRepository
+    {
+        private readonly IConfiguration _configuration;
+
+        public DiscountRepository(IConfiguration configuration)
+        {
+            _configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
+        }
+
+        public async Task<Coupon> GetDiscount(string productName)
+        {
+            using var connection = new NpgsqlConnection
+                (_configuration.GetValue<string>("DatabaseStrings:ConnectionString"));
+
+            var cooupon = await connection.QueryFirstOrDefaultAsync<Coupon>
+                ("SELECT * FROM Coupon WHERE ProductName = @ProductName", new { ProductName = productName });
+        }
+    }
+}
